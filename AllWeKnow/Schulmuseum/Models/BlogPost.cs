3e65f3995031f020e2bbc5d204b515c3068ce8fd
Adrian Messner <adrian.messner08@hotmail.com>
2023-09-30 12:02:15 +0200

    Added Blog from database system, sorting system and blogview

--- AllWeKnow/Schulmuseum/Models/BlogPost.cs
+++ AllWeKnow/Schulmuseum/Models/BlogPost.cs
@@ -3,6 +3,7 @@ namespace Schulmuseum.Models;
 public class BlogPost
 {
     public int? Id { get; set; }
+    public int CategoryId { get; set; }
     public string? Title { get; set; }
     public string? TitleImage { get; set; }
     public string? Username { get; set; }
