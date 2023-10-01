ab3bc76f18e92119574823525a4c73d19a310086
Moritz <moritz.deiaco@gmail.com>
2023-10-01 02:19:20 +0200

    some small changes

--- AllWeKnow/Schulmuseum/Program.cs
+++ AllWeKnow/Schulmuseum/Program.cs
@@ -1,14 +1,14 @@
 using DatabaseLibrary;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Schulmuseum.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
 builder.Services.AddSingleton<DataAccess>();
 builder.Services.AddHttpClient();
 
