e90a6861df7785eae13b2ad0bbd8e6770f6131ef
Adrian Messner <adrian.messner08@hotmail.com>
2023-10-06 18:51:10 +0200

    Added random joke system

---
+++ AllWeKnow/Schulmuseum/JokeGenerator.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+
+namespace Schulmuseum;
+
+public static class JokeGenerator
+{
+    public static string GetJoke()
+    {
+        try
+        {
+            // Open the text file using a stream reader.
+            using (var sr = new StreamReader("wwwroot/resources/loading/loadingjokes.txt"))
+            {
+                int count = 0;
+                List<string> lines = new List<string>();
+                Random r = new Random();  
+                
+                while (sr.ReadLine() is { } line)
+                {
+                    count++;
+                    lines.Add(line);
+                }
+                return lines[r.Next(0, count)];
+            }
+        }
+        catch (IOException e)
+        {
+            return e.Message;
+        }
+    }
+}
