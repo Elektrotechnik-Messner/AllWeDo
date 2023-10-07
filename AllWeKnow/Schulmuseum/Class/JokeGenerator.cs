021a9f228f7c23a97af8ab1c4e682e583ed4eebe
Moritz <moritz.deiaco@gmail.com>
2023-10-07 10:03:00 +0200

    added some short loading jokes and some other things

--- AllWeKnow/Schulmuseum/Class/JokeGenerator.cs
+++ AllWeKnow/Schulmuseum/Class/JokeGenerator.cs
@@ -29,4 +29,29 @@ public static class JokeGenerator
             return e.Message;
         }
     }
+
+    public static string GetShortJoke()
+    {
+        try
+        {
+            // Open the text file using a stream reader.
+            using (var sr = new StreamReader("wwwroot/resources/loading/loadingjokesshort.txt"))
+            {
+                int count = 0;
+                List<string> lines = new List<string>();
+                Random r = new Random();  
+                
+                while (sr.ReadLine() is { } line)
+                {
+                    count++;
+                    lines.Add(line);
+                }
+                return lines[r.Next(0, count)];
+            }
+        }
+        catch (IOException e)
+        {
+            return e.Message;
+        }
+    }
 }
