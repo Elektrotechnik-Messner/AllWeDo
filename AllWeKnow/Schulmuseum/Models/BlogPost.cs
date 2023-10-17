c77f1325bb9895d85f147991420d4109f127f007
Adrian Messner <adrian.messner08@hotmail.com>
2023-10-17 17:59:32 +0200

    Random post

--- AllWeKnow/Schulmuseum/Models/BlogPost.cs
+++ AllWeKnow/Schulmuseum/Models/BlogPost.cs
@@ -5,6 +5,7 @@ public class BlogPost
     public int? Id { get; set; }
     public int CategoryId { get; set; }
     public string? Title { get; set; }
+    public string? Description { get; set; }
     public string? TitleImage { get; set; }
     public string? Username { get; set; }
     public string? Date { get; set; }
