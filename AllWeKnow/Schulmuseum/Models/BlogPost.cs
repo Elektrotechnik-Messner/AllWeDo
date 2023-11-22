7b43e8e682cf21fb0ce816349148151cf4507eb8
Adrian Messner <adrian.messner08@hotmail.com>
2023-11-22 20:51:59 +0100

    Added delete from db at meine-artikel; Almost finished status system

--- AllWeKnow/Schulmuseum/Models/BlogPost.cs
+++ AllWeKnow/Schulmuseum/Models/BlogPost.cs
@@ -3,13 +3,27 @@ namespace Schulmuseum.Models;
 public class BlogPost
 {
     public int Id { get; set; }
-    public int Qualified { get; set; }
+    public int Status { get; set; }
     public int CategoryId { get; set; }
     public int CreatorId { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
     public string? TitleImage { get; set; }
-    public string? Username { get; set; }
+    
     public string? Date { get; set; }
     public string? Content { get; set; }
 }
+
+
+/*
+Status:
+    - -1: Nicht genehmigt(muss überarbeitet werden) 
+    -  0: Noch nicht genehmigt 
+    -  1: Genehmigt
+    -  2: wird bearbeitet   (verboten)
+    - 31: Wird korrigiert   (Nicht genehmigt) von der Lehrperson
+    - 32: Wird bearbeitet   (Nicht genehmigt) vom Schüler
+    - 41: Wird korrigiert   (Genehmigt)
+    - 42: Wird bearbeitet   (Genehmigt)
+    -  5: Qualified by admin
+*/
